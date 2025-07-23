d90b809c6b5cbe91961e2f24f3a80f9d6d403a32
prabhu-woyce <pparmar@woyce.io>
2025-07-23 11:57:37 +0530

    #update Add Pages

---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <div>
+        <h1>My Profile Page</h1>
+        <p>Welcome to your profile!</p>
+    </div>
+  )
+}
+
+export default MyProfile
