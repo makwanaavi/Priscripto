2df749d857598f8ddb557e6122bf63ca232e4921
prabhu-woyce <pparmar@woyce.io>
2025-07-23 16:51:18 +0530

    #update Add Banner.jsx

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { doctors } from "../assets/assets";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
-const TopDoctors = () => {  
+const TopDoctors = () => {
   const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium ">Top Doctors to Book</h1>
@@ -30,7 +31,15 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button onClick={() => navigate("/doctors")} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">See All Doctors</button>
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        See All Doctors
+      </button>
     </div>
   );
 };
